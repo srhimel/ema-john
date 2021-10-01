9f27341ef8fe3f389df885ed551fa8741bbc2fa4
srhimel <himusr@gmail.com>
2021-10-02 00:57:30 +0600

    Added router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import Inventory from './components/Inventory/Inventory';
+import OrderReview from './components/OrderReview/OrderReview';
 import Shop from './components/Shop/Shop';
 
 function App() {
   return (
     <div>
-      <Header></Header>
-      <Shop></Shop>
+      <BrowserRouter>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/">
+            <Shop></Shop>
+          </Route>
+          <Route path="/shop">
+            <Shop></Shop>
+          </Route>
+          <Route path="/review">
+            <OrderReview />
+          </Route>
+          <Route path="/inventory">
+            <Inventory />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
